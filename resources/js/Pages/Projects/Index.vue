c45dd1249416c147c5953666240420fc97674c04
unknown <csesujon155@gmail.com>
2024-10-12 00:12:14 -0700

    New Skill, New Project Create

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 </script>
 <template>
     <Head title="Projects " />
@@ -16,7 +16,9 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                Table
+                <div class="flex justify-end m-2 p-2">
+                    <Link :href="route('projects.create')" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">New Project</Link>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>

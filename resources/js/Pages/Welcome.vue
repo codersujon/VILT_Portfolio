cb0b847b5e22e5808b4090c65a07397536498469
unknown <csesujon155@gmail.com>
2024-10-13 17:50:50 -0700

    Promote, About

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,7 +2,8 @@
 import { Head, Link } from '@inertiajs/vue3';
 import FrontendLayout from '@/Layouts/Frontend.vue';
 import Hero from '@/Components/Frontend/Hero.vue';
-
+import Promote from '@/Components/Frontend/Promote.vue';
+import About from '@/Components/Frontend/About.vue';
 </script>
 
 <template>
@@ -11,7 +12,9 @@ import Hero from '@/Components/Frontend/Hero.vue';
         <!-- Hero Primary -->
          <Hero />
         <!-- promote tail-100 -->
+         <Promote />
         <!-- About Secondary-->
+         <About />
         <!-- Skills Tail-100-->
         <!-- Portfolio Primary-->
         <!-- Services Secondary-->
